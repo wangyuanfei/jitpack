27e6168f439fd66de190d7e5cdbd85d616fb0cee
yuanfei.wang <wyf-shenghuo@163.com>
2017-08-18 18:53:49 +0800

    first

---
+++ computer/src/main/java/com/example/computer/ComputerUtils.java
@@ -0,0 +1,11 @@
+package com.example.computer;
+
+/**
+ * Created by wangyuanfei on 2017/8/18.
+ */
+
+public class ComputerUtils {
+    public int addition(int a, int b) {
+        return a + b;
+    }
+}
